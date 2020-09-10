07c90cb90eaa8192711044cf232deea05418accd
Dmitri <griizly@rambler.ru>
2020-09-10 18:17:26 +0300

    First commit

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -14,34 +14,73 @@ const timeRun = (timeValue) => {
 // setInterval(timeRun, 1000)
 
 // Обратный счетчик hh:mm:ss 
-time = new Date(2020, 8, 10, 15, 45, 30);
+time = new Date(2020, 8, 10, 15, 0, 5);
+
+seconds = time.getSeconds();
+minutes = time.getMinutes();
+hours = time.getHours()
+
+if (hours.toString().length < 2) {
+    hours = "0" + hours;
+} if (minutes.toString().length < 2) {
+    minutes = "0" + minutes;
+} if (seconds.toString().length < 2) {
+    seconds = "0" + seconds;
+}
+
+document.getElementById("timer").textContent = `${hours}:${minutes}:${seconds}`;; // установка начального таймера
 
 const bigCounter = () => {
-    seconds = time.getSeconds();
-    minutes = time.getMinutes();
-    hours = time.getHours()
-
+    
     newSeconds = seconds;
     newMinutes = minutes;
     newHours = hours;
 
-    if (newSeconds !== 0) {
+    if (newHours.toString().length < 2) {
+        newHours = "0" + newHours;
+    } if (newMinutes.toString().length < 2) {
+        newMinutes = "0" + newMinutes;
+    } if (newSeconds.toString().length < 2) {
+        newSeconds = "0" + newSeconds;
+    }
+
+    if (newSeconds !== -1) {
         newSeconds -= 1;
-    } else if (newSeconds === 0) {
-        newMinutes -= 1;
+    } else if (newSeconds < 0) {
         newSeconds = 59;
-    } else if (newMinutes === 0) {
-        newHours -= 1;
+    } 
+    
+    if (newSeconds == -1 && newMinutes !== 0) {
+        newSeconds = 59;
+        newMinutes -= 1;
+    }
+    if (newSeconds == -1 && newMinutes == 0) {
         newMinutes = 59;
+        newSeconds = 59;
+    } 
+    
+    if (newSeconds == 59 && newMinutes == 59) {
+        newHours -= 1;
+       }
+    
+       if (newHours === 0 && newMinutes === 0 && newSeconds === 0) {
+        alert("Вы победили в конкурсе!");
+        return;
+    }
+
+    if (newHours.toString().length < 2) {
+        newHours = "0" + newHours;
+    } if (newMinutes.toString().length < 2) {
+        newMinutes = "0" + newMinutes;
+    } if (newSeconds.toString().length < 2) {
+        newSeconds = "0" + newSeconds;
     }
     
-    fullTime = `${newHours}:${newMinutes}:${newSeconds}`;
-
     seconds = newSeconds;
     minutes = newMinutes;
     hours = newHours;
 
-    document.getElementById("timer").textContent = fullTime;
+    document.getElementById("timer").textContent = `${newHours}:${newMinutes}:${newSeconds}`;
 
 
 }
