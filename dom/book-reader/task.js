617eb7bbe1d06acc302d429130a57c972eb77d64
Dmitri <griizly@rambler.ru>
2020-10-09 17:21:17 +0300

    Task done!

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -2,47 +2,46 @@ let fonts = document.querySelectorAll('.font-size');
 
 let textColor = document.querySelectorAll('.book__control_color a')
 
-let textActive = document.querySelector('.book__control_color').querySelector('.color_active')
-
 let bgColor = document.querySelectorAll('.book__control_background a')
-
-let bgActive = document.querySelector('.book__control_background').querySelector('.color_active')
 
 let book = document.querySelector('.book__content');
 
-for (let i = 0; i < fonts.length; i++) {
-    fonts[i].addEventListener('click', (e) => {
+for (let item of textColor) {
+    item.addEventListener('click' , (e) => {
+
+        e.preventDefault();
+        document.querySelector('.book__control_color').querySelector('.color_active').classList.remove('color_active')
+        item.classList.add('color_active');
+
+        book.classList.remove('book_color-gray', 'book_color-black', 'book_color-whitesmoke');
+        book.classList.add(`book_color-${item.getAttribute('data-text-color')}`);
+
+    })
+}
+
+
+for (let item of bgColor) {
+    item.addEventListener('click' , (e) => {
+
+        e.preventDefault();
+        document.querySelector('.book__control_background').querySelector('.color_active').classList.remove('color_active')
+        item.classList.add('color_active');
+
+        book.classList.remove('book_bg-gray', 'book_bg-black', 'book_bg-white');
+        book.classList.add(`book_bg-${item.getAttribute('data-bg-color')}`);
+    })
+}
+
+
+for (let item of fonts) {
+    item.addEventListener('click', (e) => {
 
         e.preventDefault();
         document.querySelector('.font-size_active').classList.remove('font-size_active');
-        fonts[i].classList.add('font-size_active');
+        item.classList.add('font-size_active');
 
-        book.classList.remove(book.classList.item(1));
-        book.classList.add(`book_fs-${fonts[i].getAttribute('data-size')}`);
+        book.classList.remove('book_fs-big', 'book_fs-small');
+        book.classList.add(`book_fs-${item.getAttribute('data-size')}`);
 
-    })
-}
-
-for (let i = 0; i < textColor.length; i++) {
-    textColor[i].addEventListener('click' , (e) => {
-        e.preventDefault();
-        textActive.classList.remove('color_active');
-        textColor[i].classList.add('color_active');
-        textActive = document.querySelector('.book__control_color').querySelector('.color_active');
-
-        book.classList.remove(book.classList.item(2));
-        book.classList.add(`book_color-${textColor[i].getAttribute('data-text-color')}`);
-    })
-}
-
-for (let i = 0; i < bgColor.length; i++) {
-    bgColor[i].addEventListener('click' , (e) => {
-        e.preventDefault();
-        bgActive.classList.remove('color_active');
-        bgColor[i].classList.add('color_active');
-        bgActive = document.querySelector('.book__control_background').querySelector('.color_active');
-
-        book.classList.remove(book.classList.item(3));
-        book.classList.add(`book_bg-${bgColor[i].getAttribute('data-bg-color')}`);
     })
 }
